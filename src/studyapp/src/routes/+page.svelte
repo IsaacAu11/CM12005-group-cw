75eadbcb0bec30d5664fbb9f68d9c34faccf8022
thompson <hi.thompson@hotmail.com>
2024-03-26 19:43:11 +0000

    feat(homepage): New Location Modal Done

--- src/studyapp/src/routes/+page.svelte
+++ src/studyapp/src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import Timer from '../components/Timer.svelte';
 	import create from '$lib/assets/create.svg';
+	import close from '$lib/assets/close.svg';
+	import { onMount } from 'svelte';
 	let inStudyMode: boolean = true;
 	let selectedLocation: string = '';
+	let locationModalOpen = false;
+	let newStudyLocation: string = '';
+	let savedLocations: string[] = [];
+    let rerenderSavedLocations = 0;
 
 	function toggleStudyMode() {
 		inStudyMode = !inStudyMode;
@@ -12,9 +18,24 @@
 
 	$: selectText = selectedLocation ? `${selectedLocation}` : 'Your Saved Study Locations';
 
+	onMount(async () => {
+		savedLocations = await getSavedLocations();
+	});
+
 	async function getSavedLocations() {
 		await new Promise((r) => setTimeout(r, 500));
 		return ['Library', 'Home', 'Cafe'];
+	}
+
+	async function validateAndSubmit() {
+		if (newStudyLocation) {
+			await new Promise((r) => setTimeout(r, 500));
+			savedLocations.push(newStudyLocation);
+            rerenderSavedLocations++;
+			// TODO: Insert new location into database
+			newStudyLocation = '';
+			locationModalOpen = false;
+		}
 	}
 </script>
 
@@ -24,19 +45,19 @@
 			<div class="grid grid-cols-2 gap-2">
 				<select class="select select-accent w-full max-w-xs bg-accent">
 					<option disabled selected>{selectText}</option>
-					{#await getSavedLocations()}
-						<option disabled>Loading Saved Location...</option>
-					{:then locations}
-						{#each locations as location}
+                    <!-- NOTE: Needed to use extra variable because key doesnt rerender on list push -->
+					{#key rerenderSavedLocations}
+						{#each savedLocations as location}
 							<option value={location} on:click={() => (selectedLocation = location)}
 								>{location}</option
 							>
 						{/each}
-					{:catch error}
-						<option disabled>{error}</option>
-					{/await}
+					{/key}
 				</select>
-				<button class="btn btn-accent aspect-square border-transparent">
+				<button
+					class="btn btn-accent aspect-square border-transparent"
+					on:click={() => (locationModalOpen = true)}
+				>
 					<img class="h-8" src={create} alt="create icon" />
 				</button>
 			</div>
@@ -47,7 +68,33 @@
 			{/if}
 		</div>
 		{#key inStudyMode}
-			<Timer inStudyMode={inStudyMode} />
+			<Timer {inStudyMode} />
 		{/key}
 	</div>
 </div>
+<dialog class="modal" class:modal-open={locationModalOpen}>
+	<div class="modal-box relative max-w-none w-1/3">
+		<div class="flex flex-row justify-between items-center">
+			<h1 class="font-bold text-2xl">NEW LOCATION</h1>
+			<div class="modal-action m-0">
+				<button on:click={() => (locationModalOpen = false)}>
+					<img class="h-8" src={close} alt="close icon" />
+				</button>
+			</div>
+		</div>
+		<p class="py-4">
+			Found a new study oasis? Whether it's a bustling coffee shop, a peaceful corner of your home,
+			or a scenic outdoor spot, the possibilities are endless. Let's make studying an exciting
+			journey!
+		</p>
+		<form class="grid grid-cols gap-4">
+			<input
+				type="text"
+				placeholder="ENTER THE NAME OF YOUR NEW STUDY OASIS"
+				class="input input-bordered w-full"
+				bind:value={newStudyLocation}
+			/>
+			<button class="btn" on:click={() => validateAndSubmit()}>Submit</button>
+		</form>
+	</div>
+</dialog>
